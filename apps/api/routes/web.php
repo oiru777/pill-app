b32517aa0675e13daf02e2e2cfc05c8737d64953
oiru777 <warisara11037@gmail.com>
2025-10-02 16:49:08 +0900

    ルート追加

--- apps/api/routes/web.php
+++ apps/api/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect(env('FRONTEND_URL') . '/verified');
+})->middleware(['signed'])->name('verification.verify');
+
+// フロントエンドのすべてのリクエストをキャッチするルート
+Route::get('{any}', function () {
+    return view('app');
+})->where('any','.*');
+
+Route::get('/verified', function () {
+    return 'メール認証済みです！';
 });
