f62f45afae04bd52344dc39249f288e5dd01c0a0
oiru777 <warisara11037@gmail.com>
2025-10-21 10:38:41 +0900

    mui->chakra

--- apps/web/src/pages/Verified.tsx
+++ apps/web/src/pages/Verified.tsx
@@ -1,13 +1,13 @@
 // VerifiedPage.tsx
 import React from "react";
-import { Typography, Box } from "@mui/material";
+import { Box, Heading } from "@chakra-ui/react";
 
 export const Verified = () => {
   return (
-    <Box sx={{ mt: 4, textAlign: "center" }}>
-      <Typography variant="h5" gutterBottom>
+    <Box textAlign="center" mt={8}>
+      <Heading as="h3" size="md" mb={3}>
         メール認証が完了しました
-      </Typography>
+      </Heading>
     </Box>
   );
 };
